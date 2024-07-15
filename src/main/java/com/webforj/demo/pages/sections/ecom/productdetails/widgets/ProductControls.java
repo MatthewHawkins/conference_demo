40fc50a0aff776a17cd4f21db10e6bc15b65355d
bryanhighhill <bhighhill@basis.cloud>
2024-07-15 10:30:15 -0600

    cleaned up indentation and method stringing

--- src/main/java/com/webforj/demo/pages/sections/ecom/productdetails/widgets/ProductControls.java
+++ src/main/java/com/webforj/demo/pages/sections/ecom/productdetails/widgets/ProductControls.java
@@ -9,52 +9,51 @@ import com.webforj.component.list.ListItem;
 
 public class ProductControls extends Div {
     
-    public ProductControls() {
-        ChoiceBox sizeDrop = new ChoiceBox();
+  public ProductControls() {
+    ChoiceBox sizeDrop = new ChoiceBox();
 
-        ArrayList<ListItem> sizeCategories = new ArrayList<>();
-        sizeCategories.add(new ListItem("XS"));
-        sizeCategories.add(new ListItem("S"));
-        sizeCategories.add(new ListItem("M"));
-        sizeCategories.add(new ListItem("L"));
-        sizeCategories.add(new ListItem("XL"));
+    ArrayList<ListItem> sizeCategories = new ArrayList<>();
+    sizeCategories.add(new ListItem("XS"));
+    sizeCategories.add(new ListItem("S"));
+    sizeCategories.add(new ListItem("M"));
+    sizeCategories.add(new ListItem("L"));
+    sizeCategories.add(new ListItem("XL"));
 
-        sizeDrop.insert(sizeCategories)
-            .setLabel("Size")
-            .selectIndex(0);
+    sizeDrop.insert(sizeCategories)
+      .setLabel("Size")
+      .selectIndex(0);
 
-        ChoiceBox quantityDrop = new ChoiceBox();
+    ChoiceBox quantityDrop = new ChoiceBox();
 
-        ArrayList<ListItem> quantityCategories = new ArrayList<>();
-        quantityCategories.add(new ListItem("1"));
-        quantityCategories.add(new ListItem("2"));
-        quantityCategories.add(new ListItem("3"));
-        quantityCategories.add(new ListItem("4"));
-        quantityCategories.add(new ListItem("5"));
+    ArrayList<ListItem> quantityCategories = new ArrayList<>();
+    quantityCategories.add(new ListItem("1"));
+    quantityCategories.add(new ListItem("2"));
+    quantityCategories.add(new ListItem("3"));
+    quantityCategories.add(new ListItem("4"));
+    quantityCategories.add(new ListItem("5"));
 
-        quantityDrop.insert(quantityCategories)
-            .setLabel("Quantity")
-            .selectIndex(0);
+    quantityDrop.insert(quantityCategories)
+      .setLabel("Quantity")
+      .selectIndex(0);
 
-        Div menuButtons = new Div();
-        Div optionButtons = new Div();
-        Button likeButton = new Button("""
-            <html>
-            <dwc-icon name='heart' expanse='s'></dwc-icon>
-            </html>
-        """);
+    Div menuButtons = new Div();
+    Div optionButtons = new Div();
+    Button likeButton = new Button("""
+      <html>
+      <dwc-icon name='heart' expanse='s'></dwc-icon>
+      </html>
+    """)
+      .addClassName("like-button");
 
-        likeButton.addClassName("like-button");
+    Button cartButton = new Button("Add To Cart")
+      .addClassName("add-to-cart-button");
 
-        Button cartButton = new Button("Add To Cart");
-        cartButton.addClassName("add-to-cart-button");
+    menuButtons.add(sizeDrop, quantityDrop);
+    optionButtons.add(likeButton, cartButton);
+    menuButtons.addClassName("control-buttons-container");
+    optionButtons.addClassName("control-buttons-container");
 
-        menuButtons.add(sizeDrop, quantityDrop);
-        optionButtons.add(likeButton, cartButton);
-        menuButtons.addClassName("control-buttons-container");
-        optionButtons.addClassName("control-buttons-container");
-
-        this.add(menuButtons, optionButtons);
-        this.addClassName("controls-container");
-    }
+    this.add(menuButtons, optionButtons);
+    this.addClassName("controls-container");
+  }
 }
