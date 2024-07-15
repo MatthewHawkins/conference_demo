fd0917004d8af1d71ef25ed371b9d49a3df22452
bryanhighhill <bhighhill@basis.cloud>
2024-07-15 10:47:01 -0600

    Cleaned up indentation

--- src/main/java/com/webforj/demo/pages/sections/ecom/productdetails/widgets/ProductInfo.java
+++ src/main/java/com/webforj/demo/pages/sections/ecom/productdetails/widgets/ProductInfo.java
@@ -4,28 +4,28 @@ import com.webforj.component.html.elements.Div;
 import com.webforj.component.html.elements.Paragraph;
 import com.webforj.component.html.elements.Span;
 
-public class ProductInfo extends Div{
+public class ProductInfo extends Div {
 
-    public ProductInfo(Paragraph title, Paragraph rating, Paragraph stockStatus, String originalPrice, String discountedPrice, String description) {
-        title.addClassName("product-title");
-        rating.addClassName("product-rating");
-        stockStatus.addClassName("stock-status");
+  public ProductInfo(Paragraph title, Paragraph rating, Paragraph stockStatus, String originalPrice, String discountedPrice, String description) {
+    title.addClassName("product-title");
+    rating.addClassName("product-rating");
+    stockStatus.addClassName("stock-status");
 
-        Span originalPriceSpan = new Span(originalPrice);
-        originalPriceSpan.addClassName("original-price");
-        Span discountedPriceSpan = new Span(discountedPrice);
-        discountedPriceSpan.addClassName("discounted-price");
+    Span originalPriceSpan = new Span(originalPrice);
+    originalPriceSpan.addClassName("original-price");
+    Span discountedPriceSpan = new Span(discountedPrice);
+    discountedPriceSpan.addClassName("discounted-price");
 
-        Paragraph price = new Paragraph("Price: ");
-        price.add(originalPriceSpan, discountedPriceSpan);
-        price.addClassName("product-price");
+    Paragraph price = new Paragraph("Price: ");
+    price.add(originalPriceSpan, discountedPriceSpan);
+    price.addClassName("product-price");
 
-        Div descContainer = new Div();
-        descContainer.setText(description)
-            .addClassName("product-description");
+    Div descContainer = new Div();
+    descContainer.setText(description)
+        .addClassName("product-description");
 
-        this.add(title, rating, stockStatus, price, descContainer);
-        this.addClassName("product-info-container");
-    }
+    this.add(title, rating, stockStatus, price, descContainer);
+    this.addClassName("product-info-container");
+  }
     
 }
