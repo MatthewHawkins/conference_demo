0c8e8a5df9fb9bce58400629db2aee4a4baaed47
bryanhighhill <bhighhill@basis.cloud>
2024-07-15 10:08:55 -0600

    instantiated method variables on declaration & cleaned up indentation

--- src/main/java/com/webforj/demo/pages/sections/ecom/checkout/widgets/InfoControls.java
+++ src/main/java/com/webforj/demo/pages/sections/ecom/checkout/widgets/InfoControls.java
@@ -7,49 +7,50 @@ import com.webforj.component.html.elements.Div;
 
 public class InfoControls extends Composite<Div> {
     
-    private Button billingBtn;
-    private Button shippingBtn;
-    private Button paymentBtn;
+  private Button billingBtn = new Button();
+  private Button shippingBtn = new Button();
+  private Button paymentBtn = new Button();
 
-    public InfoControls() {
-        billingBtn = new Button("""
-            <html>
-                <div style='text-align: center'>
-                    <dwc-icon name='user' style='display: block; margin: auto; margin-bottom: 10px;'></dwc-icon>
-                    <span style='display: block; margin-top: 10px;'>
-                        Billing Info
-                    </span>
-                </div>
-            </html>
-        """).setTheme(ButtonTheme.INFO);
-        billingBtn.addClassName("checkout-step");
+  public InfoControls() {
+    billingBtn.setText("""
+      <html>
+        <div style='text-align: center'>
+          <dwc-icon name='user' style='display: block; margin: auto; margin-bottom: 10px;'></dwc-icon>
+          <span style='display: block; margin-top: 10px;'>
+            Billing Info
+          </span>
+        </div>
+      </html>
+    """)
+      .setTheme(ButtonTheme.INFO)
+      .addClassName("checkout-step");
 
-        shippingBtn = new Button("""
-            <html>
-                <div style='text-align: center'>
-                    <dwc-icon name='truck-delivery' style='display: block; margin: auto; margin-bottom: 10px;'></dwc-icon>
-                    <span style='display: block; margin-top: 10px;'>
-                        Shipping Info
-                    </span>
-                </div>
-            </html>
-        """);
-        shippingBtn.addClassName("checkout-step");
+    shippingBtn.setText("""
+      <html>
+        <div style='text-align: center'>
+          <dwc-icon name='truck-delivery' style='display: block; margin: auto; margin-bottom: 10px;'></dwc-icon>
+          <span style='display: block; margin-top: 10px;'>
+            Shipping Info
+          </span>
+        </div>
+      </html>
+    """)
+      .addClassName("checkout-step");
 
-        paymentBtn = new Button("""
-            <html>
-                <div style='text-align: center'>
-                    <dwc-icon name='cash' style='display: block; margin: auto; margin-bottom: 10px;'></dwc-icon>
-                    <span style='display: block; margin-top: 10px;'>
-                        Payment Info
-                    </span>
-                </div>
-            </html>        
-        """);
-        paymentBtn.addClassName("checkout-step");
-    
-        getBoundComponent().addClassName("info-controls-container")
-            .add(billingBtn, shippingBtn, paymentBtn);
-    }
+    paymentBtn.setText("""
+      <html>
+        <div style='text-align: center'>
+          <dwc-icon name='cash' style='display: block; margin: auto; margin-bottom: 10px;'></dwc-icon>
+          <span style='display: block; margin-top: 10px;'>
+            Payment Info
+          </span>
+        </div>
+      </html>        
+    """)
+      .addClassName("checkout-step");
+  
+    getBoundComponent().addClassName("info-controls-container")
+      .add(billingBtn, shippingBtn, paymentBtn);
+  }
     
 }
