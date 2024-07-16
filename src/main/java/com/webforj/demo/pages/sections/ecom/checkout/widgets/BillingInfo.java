48a6384bb553fd448e0fd7b6126da4e0695460cc
bryanhighhill <bhighhill@basis.cloud>
2024-07-16 15:23:09 -0600

    Added functionality: When user selects "Ship to a different address" an additional section appears with inputs for additional mailing address. These extra inputs toggle visibility with the "Ship to a different address" button

--- src/main/java/com/webforj/demo/pages/sections/ecom/checkout/widgets/BillingInfo.java
+++ src/main/java/com/webforj/demo/pages/sections/ecom/checkout/widgets/BillingInfo.java
@@ -1,47 +1,60 @@
 package com.webforj.demo.pages.sections.ecom.checkout.widgets;
 
+import com.webforj.App;
 import com.webforj.component.button.Button;
 import com.webforj.component.button.ButtonTheme;
+import com.webforj.component.event.ToggleEvent;
 import com.webforj.component.field.TextField;
 import com.webforj.component.html.elements.Div;
 import com.webforj.component.html.elements.Paragraph;
 import com.webforj.component.layout.flexlayout.FlexLayout;
 import com.webforj.component.list.ChoiceBox;
 import com.webforj.component.maskednumberfield.MaskedNumberField;
-import com.webforj.component.optioninput.RadioButton;
+import com.webforj.component.optioninput.CheckBox;
 
-public class BillingInfo extends Div{
+public class BillingInfo extends Div {
   private Paragraph title = new Paragraph("Billing Information");
+  private Paragraph mailingTitle = new Paragraph("Mailing Information");
   private Paragraph subtitle = new Paragraph("Fill in the form below to receive an invoice for your order.");
-  private TextField firstName = new TextField();
-  private TextField lastName = new TextField();
-  private TextField email = new TextField();
-  private TextField phone = new TextField();
-  private TextField address = new TextField();
-  private TextField city = new TextField();
-  private TextField orderNotes = new TextField();
+  private Paragraph mailingSubtitle = new Paragraph("Please fill in your mailing information");
+  private TextField firstName = new TextField("First Name");
+  private TextField lastName = new TextField("Last Name");
+  private TextField email = new TextField("Email");
+  private TextField phone = new TextField("Phone");
+  private TextField address = new TextField("Address");
+  private TextField city = new TextField("City");
+  private TextField orderNotes = new TextField("Order Notes");
   private ChoiceBox states = new ChoiceBox();
+  private ChoiceBox mailingStates = new ChoiceBox();
   private ChoiceBox countries = new ChoiceBox();
+  private ChoiceBox mailingCountries = new ChoiceBox();
   private MaskedNumberField zip = new MaskedNumberField();
-  private RadioButton diffAddress = new RadioButton();
+  private CheckBox diffAddress = new CheckBox();
   private Button submit = new Button();
   private Button back = new Button();
+  private TextField mailingFirstName = new TextField("First Name");
+  private TextField mailingLastName = new TextField("Last Name");
+  private TextField mailingEmail = new TextField("Email");
+  private TextField mailingPhone = new TextField("Phone");
+  private TextField mailingAddress = new TextField("Address");
+  private TextField mailingCity = new TextField("City");
+  private MaskedNumberField mailingZip = new MaskedNumberField();
+  private FlexLayout mailingLayout;
+  private FlexLayout mainLayout;
+  private FlexLayout footerLayout;
 
   public BillingInfo() {
-    firstName.setLabel("First Name");
-    lastName.setLabel("Last Name");
-    email.setLabel("Email");
-    phone.setLabel("Phone");
-    address.setLabel("Address");
-    city.setLabel("City");
+    this.addClassName("billing-info");
+
     zip.setAttribute("label", "Zip Code");
 
     populateStates();
     populateCountries();
 
+    //make a checkbox instead of radio button
     diffAddress.setText("Ship to different address?");
-    orderNotes.setPlaceholder("Write order notes...")
-      .setLabel("Order Notes");
+    diffAddress.addToggleListener(this::addMailingInfo);
+    orderNotes.setPlaceholder("Write order notes...");
 
     back.setText("""
       <html>
@@ -64,132 +77,212 @@ public class BillingInfo extends Div{
     """)
       .setTheme(ButtonTheme.SUCCESS)
       .addClassName("button-submit");
-
-    FlexLayout mainLayout = FlexLayout.create()
-      .vertical() 
-      .build()
-      .addClassName("billing-info")
-      .setStyle("gap", "40px");
-
-    FlexLayout rowOne = FlexLayout.create(title, subtitle)
+      
+      FlexLayout rowOne = FlexLayout.create(title, subtitle)
       .vertical()
       .build();
-    title.setStyle("font-size", "16px");
-    subtitle.setStyle("font-size", "12px")
+      title.setStyle("font-size", "16px");
+      subtitle.setStyle("font-size", "12px")
       .setStyle("color", "#9ba8af");    
-
-    FlexLayout rowTwo = FlexLayout.create(firstName, lastName)
+      
+      FlexLayout rowTwo = FlexLayout.create(firstName, lastName)
       .horizontal()
       .build()
       .setItemBasis("50%", firstName)
       .setItemBasis("50%", lastName);
-
-    FlexLayout rowThree = FlexLayout.create(email, phone)
+      
+      FlexLayout rowThree = FlexLayout.create(email, phone)
       .horizontal()
       .build()
       .setItemBasis("50%", email)
       .setItemBasis("50%", phone);
-
-    FlexLayout rowFour = FlexLayout.create(city, states, zip)
+      
+      FlexLayout rowFour = FlexLayout.create(city, states, zip)
       .horizontal()
       .build()
       .setItemBasis("50%", city)
       .setItemBasis("30%", states);
-    zip.setStyle("width", "20%");
-
-    FlexLayout rowFive = FlexLayout.create(back, submit)
+      zip.setStyle("width", "20%");
+      
+      FlexLayout rowFive = FlexLayout.create(back, submit)
       .horizontal()
       .justify().between()
       .build()
       .setStyle("margin-top", "30px");
-
-    mainLayout.add(rowOne, rowTwo, address, rowThree, rowFour, countries, diffAddress, orderNotes, rowFive);
       
-    Div billingInfo = new Div(mainLayout);
-    this.add(billingInfo);
+      mainLayout = FlexLayout.create(rowOne, rowTwo, address, rowThree, rowFour, countries, diffAddress)
+        .vertical() 
+        .build()
+        .addClassName("billing-info order-div-1")
+        .setStyle("gap", "40px");
+      
+      footerLayout = FlexLayout.create(rowFive)
+        .horizontal()
+        .justify().between()
+        .build()
+        .addClassName("billing-info")
+        .setStyle("margin-top", "30px");
+      
+    this.add(mainLayout, footerLayout);
+    mainLayout.setStyle("order", "1");
+    footerLayout.setStyle("order", "3");
   }
 
   private void populateCountries() {
-    countries.setLabel("Country").setMaxRowCount(10); 
-    countries.add("US", "United States");
-    countries.add("CA", "Canada");
-    countries.add("MX", "Mexico");
-    countries.add("BR", "Brazil");
-    countries.add("AR", "Argentina");
-    countries.add("UK", "United Kingdom");
-    countries.add("FR", "France");
-    countries.add("DE", "Germany");
-    countries.add("IT", "Italy");
-    countries.add("ES", "Spain");
-    countries.add("CN", "China");
-    countries.add("JP", "Japan");
-    countries.add("IN", "India");
-    countries.add("AU", "Australia");
-    countries.add("NZ", "New Zealand");
-    countries.add("RU", "Russia");
-    countries.add("ZA", "South Africa");
-    countries.add("EG", "Egypt");
-    countries.add("NG", "Nigeria");
-    countries.add("KE", "Kenya");
-    countries.add("SA", "Saudi Arabia");
-    countries.add("AE", "United Arab Emirates");
-    countries.add("SG", "Singapore");
-    countries.add("TH", "Thailand");
-    countries.add("KR", "South Korea");
+    ChoiceBox countryDrop = new ChoiceBox();
+
+    if (diffAddress.isChecked()) {
+      countryDrop = mailingCountries;
+    } else {
+      countryDrop = countries;
+    }
+
+    countryDrop.setLabel("Country").setMaxRowCount(10); 
+    countryDrop.add("US", "United States");
+    countryDrop.add("CA", "Canada");
+    countryDrop.add("MX", "Mexico");
+    countryDrop.add("BR", "Brazil");
+    countryDrop.add("AR", "Argentina");
+    countryDrop.add("UK", "United Kingdom");
+    countryDrop.add("FR", "France");
+    countryDrop.add("DE", "Germany");
+    countryDrop.add("IT", "Italy");
+    countryDrop.add("ES", "Spain");
+    countryDrop.add("CN", "China");
+    countryDrop.add("JP", "Japan");
+    countryDrop.add("IN", "India");
+    countryDrop.add("AU", "Australia");
+    countryDrop.add("NZ", "New Zealand");
+    countryDrop.add("RU", "Russia");
+    countryDrop.add("ZA", "South Africa");
+    countryDrop.add("EG", "Egypt");
+    countryDrop.add("NG", "Nigeria");
+    countryDrop.add("KE", "Kenya");
+    countryDrop.add("SA", "Saudi Arabia");
+    countryDrop.add("AE", "United Arab Emirates");
+    countryDrop.add("SG", "Singapore");
+    countryDrop.add("TH", "Thailand");
+    countryDrop.add("KR", "South Korea");
   }
 
   private void populateStates() {
-    states.setLabel("State").setMaxRowCount(10);
-    states.add("AL", "Alabama");
-    states.add("AK", "Alaska");
-    states.add("AZ", "Arizona");
-    states.add("AR", "Arkansas");
-    states.add("CA", "California");
-    states.add("CO", "Colorado");
-    states.add("CT", "Connecticut");
-    states.add("DE", "Delaware");
-    states.add("FL", "Florida");
-    states.add("GA", "Georgia");
-    states.add("HI", "Hawaii");
-    states.add("ID", "Idaho");
-    states.add("IL", "Illinois");
-    states.add("IN", "Indiana");
-    states.add("IA", "Iowa");
-    states.add("KS", "Kansas");
-    states.add("KY", "Kentucky");
-    states.add("LA", "Louisiana");
-    states.add("ME", "Maine");
-    states.add("MD", "Maryland");
-    states.add("MA", "Massachusetts");
-    states.add("MI", "Michigan");
-    states.add("MN", "Minnesota");
-    states.add("MS", "Mississippi");
-    states.add("MO", "Missouri");
-    states.add("MT", "Montana");
-    states.add("NE", "Nebraska");
-    states.add("NV", "Nevada");
-    states.add("NH", "New Hampshire");
-    states.add("NJ", "New Jersey");
-    states.add("NM", "New Mexico");
-    states.add("NY", "New York");
-    states.add("NC", "North Carolina");
-    states.add("ND", "North Dakota");
-    states.add("OH", "Ohio");
-    states.add("OK", "Oklahoma");
-    states.add("OR", "Oregon");
-    states.add("PA", "Pennsylvania");
-    states.add("RI", "Rhode Island");
-    states.add("SC", "South Carolina");
-    states.add("SD", "South Dakota");
-    states.add("TN", "Tennessee");
-    states.add("TX", "Texas");
-    states.add("UT", "Utah");
-    states.add("VT", "Vermont");
-    states.add("VA", "Virginia");
-    states.add("WA", "Washington");
-    states.add("WV", "West Virginia");
-    states.add("WI", "Wisconsin");
-    states.add("WY", "Wyoming");
+    ChoiceBox stateDrop = new ChoiceBox();
+    
+    if (diffAddress.isChecked()) {
+      stateDrop = mailingStates;
+    } else {
+      stateDrop = states;
+    }
+
+    stateDrop.setLabel("State").setMaxRowCount(10);
+    stateDrop.add("AL", "Alabama");
+    stateDrop.add("AK", "Alaska");
+    stateDrop.add("AZ", "Arizona");
+    stateDrop.add("AR", "Arkansas");
+    stateDrop.add("CA", "California");
+    stateDrop.add("CO", "Colorado");
+    stateDrop.add("CT", "Connecticut");
+    stateDrop.add("DE", "Delaware");
+    stateDrop.add("FL", "Florida");
+    stateDrop.add("GA", "Georgia");
+    stateDrop.add("HI", "Hawaii");
+    stateDrop.add("ID", "Idaho");
+    stateDrop.add("IL", "Illinois");
+    stateDrop.add("IN", "Indiana");
+    stateDrop.add("IA", "Iowa");
+    stateDrop.add("KS", "Kansas");
+    stateDrop.add("KY", "Kentucky");
+    stateDrop.add("LA", "Louisiana");
+    stateDrop.add("ME", "Maine");
+    stateDrop.add("MD", "Maryland");
+    stateDrop.add("MA", "Massachusetts");
+    stateDrop.add("MI", "Michigan");
+    stateDrop.add("MN", "Minnesota");
+    stateDrop.add("MS", "Mississippi");
+    stateDrop.add("MO", "Missouri");
+    stateDrop.add("MT", "Montana");
+    stateDrop.add("NE", "Nebraska");
+    stateDrop.add("NV", "Nevada");
+    stateDrop.add("NH", "New Hampshire");
+    stateDrop.add("NJ", "New Jersey");
+    stateDrop.add("NM", "New Mexico");
+    stateDrop.add("NY", "New York");
+    stateDrop.add("NC", "North Carolina");
+    stateDrop.add("ND", "North Dakota");
+    stateDrop.add("OH", "Ohio");
+    stateDrop.add("OK", "Oklahoma");
+    stateDrop.add("OR", "Oregon");
+    stateDrop.add("PA", "Pennsylvania");
+    stateDrop.add("RI", "Rhode Island");
+    stateDrop.add("SC", "South Carolina");
+    stateDrop.add("SD", "South Dakota");
+    stateDrop.add("TN", "Tennessee");
+    stateDrop.add("TX", "Texas");
+    stateDrop.add("UT", "Utah");
+    stateDrop.add("VT", "Vermont");
+    stateDrop.add("VA", "Virginia");
+    stateDrop.add("WA", "Washington");
+    stateDrop.add("WV", "West Virginia");
+    stateDrop.add("WI", "Wisconsin");
+    stateDrop.add("WY", "Wyoming");
   }
 
+  private void addMailingInfo(ToggleEvent e) {
+    if (e.isToggled()) {
+      if (mailingStates.isEmpty() || mailingCountries.isEmpty()) {
+        mailingZip.setAttribute("label", "Zip Code");
+
+        populateStates();
+        populateCountries();
+
+        mailingLayout = FlexLayout.create()
+          .vertical() 
+          .build()
+          .addClassName("billing-info order-div-2")
+          .setStyle("gap", "40px");
+  
+        FlexLayout rowOne = FlexLayout.create(mailingTitle, mailingSubtitle)
+          .vertical()
+          .build();
+        mailingTitle.setStyle("font-size", "16px");
+        mailingSubtitle.setStyle("font-size", "12px")
+          .setStyle("color", "#9ba8af");    
+
+        FlexLayout rowTwo = FlexLayout.create(mailingFirstName, mailingLastName)
+          .horizontal()
+          .build()
+          .setItemBasis("50%", mailingFirstName)
+          .setItemBasis("50%", mailingLastName);
+
+        FlexLayout rowThree = FlexLayout.create(mailingEmail, mailingPhone)
+          .horizontal()
+          .build()
+          .setItemBasis("50%", mailingEmail)
+          .setItemBasis("50%", mailingPhone);
+
+        FlexLayout rowFour = FlexLayout.create(mailingCity, mailingStates, mailingZip)
+          .horizontal()
+          .build()
+          .setItemBasis("50%", mailingCity)
+          .setItemBasis("30%", mailingStates);
+        mailingZip.setStyle("width", "20%");
+
+        mailingLayout.add(rowOne, rowTwo, mailingAddress, rowThree, rowFour, mailingCountries);
+        this.add(mailingLayout);
+        mailingLayout.setStyle("order", "2");
+      } else {
+        mailingLayout.setVisible(true);
+      }
+    } else {
+      mailingLayout.setVisible(false);
+      mailingAddress.setText("");
+      mailingCity.setText("");
+      mailingEmail.setText("");
+      mailingFirstName.setText("");
+      mailingLastName.setText("");
+      mailingPhone.setText("");
+      mailingCountries.setText("");
+      mailingStates.setText("");
+    }
+  }
 }
