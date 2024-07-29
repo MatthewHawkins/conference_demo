1f29b556b507a6f4eccb2e90cfa07bb9b1d5aee3
bryanhighhill <bhighhill@basis.cloud>
2024-07-29 12:36:27 -0600

    Updated docs page with iframe of webforJ docs site

--- src/main/java/com/webforj/demo/pages/Docs.java
+++ src/main/java/com/webforj/demo/pages/Docs.java
@@ -1,8 +1,19 @@
 package com.webforj.demo.pages;
 
+import com.webforj.component.html.elements.Div;
+
 public class Docs extends BasePage<Docs>{
   
   public Docs(){
-    self.setText("Docs");
+    Div iframe = new Div();
+    iframe.setStyle("height", "100%");
+    self.add(iframe);
+    iframe.setHtml(/* html */"""
+      <iframe
+        src="https://documentation.webforj.com/"
+        style="width: 100%; height: 100%; border: none; overflow: hidden;"
+        loading="lazy">
+      </iframe>
+    """);
   }
 }
