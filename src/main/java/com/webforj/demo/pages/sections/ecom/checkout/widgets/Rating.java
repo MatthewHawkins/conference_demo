8c538597e5c5f0760f533c9523b287d15e4466a1
bryanhighhill <bhighhill@basis.cloud>
2024-07-30 16:43:12 -0600

    added Rating class so we can use shoelace rating web component

---
+++ src/main/java/com/webforj/demo/pages/sections/ecom/checkout/widgets/Rating.java
@@ -0,0 +1,20 @@
+package com.webforj.demo.pages.sections.ecom.checkout.widgets;
+
+import com.webforj.annotation.Attribute;
+import com.webforj.annotation.JavaScript;
+import com.webforj.annotation.StyleSheet;
+import com.webforj.component.element.ElementComposite;
+import com.webforj.component.element.annotation.NodeName;
+
+@StyleSheet("https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.16.0/cdn/themes/light.css")
+
+  @JavaScript(
+      value = "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0-beta.87/dist/shoelace.js",
+      attributes = {@Attribute(name = "type", value = "module")})
+  @NodeName("sl-rating")
+
+  public final class Rating extends ElementComposite {
+    public Rating() {
+      super();
+    }
+  }
