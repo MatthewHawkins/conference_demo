aa75eea5f49b8c1d2dfccd09cc6bd1ff9c88de7b
bryanhighhill <bhighhill@basis.cloud>
2024-07-31 10:26:37 -0600

    added number of customer reviews to each item card and corrected css layout centering of text properly

--- src/main/java/com/webforj/demo/pages/sections/ecom/products/widgets/ItemCard.java
+++ src/main/java/com/webforj/demo/pages/sections/ecom/products/widgets/ItemCard.java
@@ -9,25 +9,28 @@ public class ItemCard extends Composite<Div> {
   private Div nameDiv = new Div();
   private Div priceDiv = new Div();
   private Div ratingDiv = new Div();
+  private Div customerReviewsDiv = new Div();
   private Div stockDiv = new Div();
 
-  public ItemCard(String productName, Img productImage, String price, String rating, String stock) {
+  public ItemCard(String productName, Img productImage, String price, String rating, String stock, String customerReviews) {
     imageDiv.addClassName("item-card__image");
     nameDiv.addClassName("item-card__name");
     priceDiv.addClassName("item-card__price");
     ratingDiv.addClassName("item-card__rating");
+    customerReviewsDiv.addClassName("item-card__customer-reviews");
     stockDiv.addClassName("item-card__stock");
 
     this.nameDiv.setText(productName);
     this.priceDiv.setText(price);
     this.ratingDiv.setText(rating);
+    this.customerReviewsDiv.setText(customerReviews);
     this.stockDiv.setText("Stock: " + stock);
 
     imageDiv.add(productImage);
     imageDiv.setStyle("background-color", "var(--dwc-surface-1)");
 
     getBoundComponent().addClassName("item-card")
-      .add(imageDiv, nameDiv, priceDiv, ratingDiv, stockDiv);
+      .add(imageDiv, nameDiv, priceDiv, ratingDiv, customerReviewsDiv, stockDiv);
   }
 
 }
